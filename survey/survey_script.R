31fec441ccca81e2b96555e6539fbf28fef61809
Will Cornwell <w.k.cornwell@vu.nl>
2013-02-15 11:27:25 -0500

    plotting survey results

--- survey/survey_script.R
+++ survey/survey_script.R
@@ -47,6 +47,23 @@ res <- lm(Estimate.logit ~ Training + Familiarity, data=d)
 
 summary(res)
 anova(res)
-plot(d$Estimate~d$Familiarity)
+
+pdf("SurveyByExperience.pdf",width=11,height=8.5)
+par(mfrow=c(1,2),mar=c(6.5,5,2,2))
+plot(d$Estimate~d$Familiarity,col="lightgrey",cex.axis=0.75,ylab="Estimate of proportion woodiness",xlab="",axes=FALSE)
+axis(side=2,cex=1.5,cex.lab=2)
+text(seq(1,4,by=1),y=-5, labels = levels(d$Familiarity),srt = -45, xpd = TRUE,adj = c(0, NA),cex=0.9)
+abline(h=47.5,col="red",lty=1)
+polygon(x=c(0,0,5,5),y=c(46,49,49,46),col=rgb(red=1,green=0,blue=0,alpha=0.2),border=NA)
 
 
+plot(d$Estimate~d$Training,col="lightgrey",cex.axis=0.75,ylab="",xlab="",axes=FALSE)
+axis(side=2)
+par(srt=2)
+#axis(side=1,at=1:5,labels=levels(d$Training))
+xl<-c("Postgrad","Part Postgrad","Undergrad","Part Undergrad","None")
+text(1:5,y=-3, labels = xl, srt = 0, ,srt = -45, xpd = TRUE,adj = c(0, NA),cex=0.9)
+abline(h=47.5,col="red",lty=1)
+polygon(x=c(0,0,6,6),y=c(46,49,49,46),col=rgb(red=1,green=0,blue=0,alpha=0.2),border=NA)
+
+dev.off()
