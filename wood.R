9561b0f8d2127915e09c6253ec01193bbf66e2f9
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-02-20 16:15:59 +1100

    Separated colours out into variables.

--- wood.R
+++ wood.R
@@ -5,6 +5,18 @@ library(RColorBrewer)
 path.forest <- readLines("~/.forest_path")
 source("wood-functions.R")
 
+## Colours used throughout:
+cols.methods <- c(strong="#a63813", weak="#4d697f") # red, blue
+cols.tree <- c(Fern="#a63813",       # reddish brown
+               Gymnosperm="#533d0c", # dark brown
+               Angiosperm="#eeb911", # yellow (basal)
+               Monocots="#799321",   # green
+               Eudicots="#4d697f",    # light blue
+               Rest="gray15")
+cols.woody <- c(woody="#21313b", herbaceous="#ea7518")
+cols.shading <- "#eeb911"
+
+
 ## Data at the level of genus: has taxonomic information and counts of
 ## number of species that are woody, herbaceous, known, and known to
 ## exist.
@@ -125,7 +137,7 @@ fig.fraction.by.genus <- function(res.strong, res.weak) {
   label(.02, .96, "a)")
   hist.outline(h, "black", lwd=lwd)  
   
-  cols <- c("#a63813", "#4d697f") # red, blue
+  cols <- cols.methods
 
   h.strong <- hist(100*res.strong$family$mean, n=50, plot=FALSE)
   h.weak   <- hist(100*res.weak$family$mean, n=50, plot=FALSE)
@@ -155,13 +167,8 @@ fig.fraction.on.phylogeny <- function(res) {
            if ( x <= length(phy.o$tip.label) ) phy.group[[x]] else
            unique(phy.group[get.descendants(x, phy.o, TRUE)]))
   grp <- sapply(tmp, function(x) if (length(x) == 1) x else "Rest")
-  cols <- c(Fern="#a63813",       # reddish brown
-            Gymnosperm="#533d0c", # dark brown
-            Angiosperm="#eeb911", # yellow (basal)
-            Monocots="#799321",   # green
-            Eudicots="#4d697f",    # light blue
-            Rest="gray15")
-  col <- unname(cols[grp])
+
+  col <- unname(cols.tree[grp])
   col2 <- col[match(phy.o$edge[,2], seq_along(grp))]
 
   p <- structure(res$order[["mean"]], names=rownames(res$order))
@@ -184,7 +191,7 @@ fig.fraction.on.phylogeny <- function(res) {
                        y=sin(xy$theta)*r)
   xrad <- .5 * diff(par("usr")[1:2])/50
   pie <- cbind(p, 1 - p)
-  pie.col <- c("#21313b", "#ea7518")
+  pie.col <- cols.woody
 
   r <- 3/4
   r0 <- max(xy$r) * (1 + offset * (1-r)/2)
@@ -245,12 +252,12 @@ summary(res.lat)
 
 fig.survey.results <- function(d.survey, res.strong, res.weak) {
   ci <- 100*cbind(res.strong$overall, res.weak$overall)
-  cols <- c("#a63813", "#4d697f") # red, blue
+  cols <- cols.methods
   cols.tr <- diversitree:::add.alpha(cols, .5)
   
   layout(rbind(1:2), widths=c(4, 5))
   par(mar=c(6.5, 2, .5, .5), oma=c(0, 2, 0, 0))
-  plot(Estimate ~ Familiarity, d.survey, col="#eeb911", axes=FALSE,
+  plot(Estimate ~ Familiarity, d.survey, col=cols.shading, axes=FALSE,
        xlab="", ylab="", bty="l",
        ylim=c(0, 100))
   axis(2, las=1)
@@ -264,7 +271,7 @@ fig.survey.results <- function(d.survey, res.strong, res.weak) {
        border=NA)
   abline(h=ci["mean",], col=cols)
 
-  plot(Estimate ~ Training, d.survey, col="#eeb911", axes=FALSE,
+  plot(Estimate ~ Training, d.survey, col=cols.shading, axes=FALSE,
        xlab="", ylab="", bty="l", ylim=c(0, 100))
   axis(2, las=1)
   xl <- c("Postgrad","Part postgrad","Undergrad","Part undergrad", "None")
@@ -288,7 +295,7 @@ fig.distribution.raw <- function(res.strong, res.weak) {
   xlim <- c(42, 50)
   ylim <- range(h.strong$density, h.weak$density)
 
-  cols <- c("#a63813", "#4d697f") # red, blue
+  cols <- cols.methods
   
   par(mar=c(4.1, 4.1, .5, .5))
   plot(h.strong, col=cols[1], xlim=xlim, ylim=ylim, freq=FALSE, yaxt="n",
@@ -303,13 +310,13 @@ fig.distribution.raw <- function(res.strong, res.weak) {
 fig.survey.distribution <- function(d.survey, res.strong, res.weak) {
   ci <- 100*cbind(res.strong$overall, res.weak$overall)
   par(mar=c(4.1, 4.1, .5, .5), mgp=c(2.5, 1, 0))
-  hist(d.survey$Estimate, xlim=c(0, 100), las=1, col="#eeb911",
+  hist(d.survey$Estimate, xlim=c(0, 100), las=1, col=cols.shading,
        xlab="Estimate of percentage woodiness", main="",
        ylab="Number of responses")
 
   box(bty="l")
 
-  cols <- c("#a63813", "#4d697f") # red, blue
+  cols <- cols.methods
 
   usr <- par("usr")
   rect(ci["lower",], usr[3], ci["upper",], usr[4],
