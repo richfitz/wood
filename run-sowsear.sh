cae4d7cc10e6e0cbfb369ad033773ea0b4f3de0b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-03-12 15:37:30 +1100

    No longer need this wrapper

--- run-sowsear.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BASE=wood
-FILE=wood.R
-STR="library(sowsear);sowsear(\"$FILE\", \"Rmd\");knit(\"${FILE}md\")"
-Rscript -e "$STR"
-
-pandoc $BASE.md -o $BASE.html --standalone --highlight-style=tango
-pandoc $BASE.md -o $BASE.pdf
