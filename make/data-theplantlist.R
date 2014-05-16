4c09eac8d81f28465692ef9f78b403db7ba22b84
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-05-16 12:35:28 +1000

    Cosmetic change, avoiding abbreviations

--- make/data-theplantlist.R
+++ make/data-theplantlist.R
@@ -4,9 +4,9 @@ source("R/theplantlist.R")
 path <- "data/theplantlist"
 dir.create(file.path(path, "acceptedNames1.1"), FALSE)
 
-plant.list.get.group("g")
-plant.list.get.group("p")
-plant.list.get.group("a")
+plant.list.get.group("gymnosperm")
+plant.list.get.group("pteridophyte")
+plant.list.get.group("angiosperm")
 
 files <- dir(file.path(path, "acceptedNames1.1"), full.names=TRUE)
 dat <- do.call(rbind, lapply(files, load.names))
