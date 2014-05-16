1819689a869218ade410d2d04b645f2d8140e335
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-05-16 13:33:02 +1000

    Fix TPL issues
    A handful of (small) genera have the same name across families
    wrecking havok with the tree figure, but not changing the calculations.

--- make/output-genus_order_lookup.csv.R
+++ make/output-genus_order_lookup.csv.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
 source("R/load.R")
+suppressMessages(library(dplyr))
 
 ## Build the genus -> family -> order lookup table.
 
 ## We do this in two steps.
 ##   1. Get the mapping of genus -> family from The Plant List
 ##   2. Get the mapping of family -> order from Zanne et al.
-
-tpl <- load.theplantlist()
-tpl <- unique(tpl[c("genus", "family", "major.clade")])
+tpl.full <- load.theplantlist()
+tpl <- unique(tpl.full[c("genus", "family", "major.clade")])
 rownames(tpl) <- NULL
 
 ## Genus -> Family -> Order lookup table from Zanne et al., augmented
@@ -19,6 +19,40 @@ lookup2 <- read.csv("data/genus_order_lookup_extra.csv",
                     stringsAsFactors=FALSE)
 lookup <- rbind(lookup1, lookup2)
 rownames(lookup) <- NULL
+
+## There are a handful of problem cases in TPL: these are genera that
+## are duplicated but have different families.  None of these are very
+## large.
+duplicates <- sort(tpl$genus[duplicated(tpl$genus)])
+
+info <- tpl.full                %.%
+  filter(genus %in% duplicates) %.%
+  group_by(family, genus, major.clade) %.%
+  summarise(n=length(species))  %.%
+  arrange(genus, desc(n))
+
+## We'll generally keep just the largest group:
+keep <- as.data.frame(info)     %.%
+  group_by(genus)               %.%
+  summarise(family=family[[1]], major.clade=major.clade[[1]]) %.%
+  arrange(genus)
+
+## These are the species that we are ignoring (28).  This is tiny
+## compared with the other taxonomic errors in the database.
+drop <- as.data.frame(info) %.% group_by(genus) %.%
+  summarise(family=family[-1], n=n[-1]) %.% arrange(genus)
+sum(drop$n)
+
+## But rewrite a couple of these anyway:
+## * Malocarpus we have is not a cactus:
+keep$family[keep$genus == "Malocarpus"] <- "Zygophyllaceae"
+## * Ericaceae record is a tropicos error
+keep$family[keep$genus == "Oreocallis"] <- "Proteaceae"
+## * I camped under a Washingtonia filifera once (WCK)
+keep$family[keep$genus == "Washingtonia"] <- "Arecaceae"
+
+## Join this back in with the plant list, removing the duplicates.
+tpl <- rbind(tpl[!(tpl$genus %in% duplicates),], keep)
 
 ## Assign order to the Plant List families using this lookup:
 tpl$order <- lookup$order[match(tpl$family, lookup$family)]
